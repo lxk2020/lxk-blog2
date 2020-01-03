069f6c5c51fd2fd8d06716a005c65c099f16e446
lxk2020 <lxk2020@outlook.com>
2020-01-03 22:44:55 +0800

    Updates

---
+++ precache-manifest.03bc2de08aa51f07907bb91fdf95938d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443d0445c8136f249081b1a458c8363a",
+    "url": "/lxk-blog2/index.html"
+  },
+  {
+    "revision": "3746c715b67819d938c6",
+    "url": "/lxk-blog2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ab88dc770eacdbf02bb8",
+    "url": "/lxk-blog2/static/js/2.541d3579.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lxk-blog2/static/js/2.541d3579.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3746c715b67819d938c6",
+    "url": "/lxk-blog2/static/js/main.56d2879d.chunk.js"
+  },
+  {
+    "revision": "29d37d2047cf9be4c6b8",
+    "url": "/lxk-blog2/static/js/runtime-main.75f7e052.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lxk-blog2/static/media/logo.5d5d9eef.svg"
+  }
+]);
